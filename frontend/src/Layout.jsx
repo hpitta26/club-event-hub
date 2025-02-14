cb4e56b66860e479a0a2e4c65e2c3c7055d73b4b
hpitta <hpitta@students.cs.ubc.ca>
2025-02-14 15:44:57 -0500

    tweaked indentation

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
-  </Router>
+    </Router>
   );
 };
 

f0f66516cd36da539ae3f4bc5eb96642143e8799
GeorgeU029 <georgeulloa.fl@gmail.com>
2025-02-16 13:26:11 -0500

    I added Icons, added logo and set up a blueprint that I can easily change depending on what lorenzo wants

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,33 +1,42 @@
 import React from "react";
+import SearchIcon from "../assets/icons/SearchIcon";
+import PlusIcon from "../assets/icons/PlusIcon";
+import InboxIcon from "../assets/icons/InboxIcon";
+import UserIcon from "../assets/icons/UserIcon";
+import pulseLogo from "../assets/icons/pulse_logo_1.png"; 
 
 const Navbar = () => {
-    return(
-        <nav className="bg-gray-900 text-white p-4 flex justify-between items-center">
-            <h1 className="text-2xl font-bold">GatherU</h1>
-            <ul className="flex space-x-6">
-                <li>
-                    <a href="#" className="hover:text-gray-400">Events</a>
-                </li>
-                <li>
-                    <a href="#" className = "hover:text-gray-400">Followed</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:text-gray-400">Discover</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:text-gray-400">+</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:text-gray-400">Search</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:text-gray-400">Notifcation</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:text-gray-400">User</a>
-                </li>
+    return (
+        <nav className="bg-[#1F1F1F] text-white p-4 flex items-center justify-between"> {}
+            {/* Logo (Left) */}
+            <div className="flex items-center">
+                <img src={pulseLogo} alt="GatherU Logo" className="h-8 w-auto" /> {}
+            </div>
+
+            {/* Center Nav Links */}
+            <ul className="hidden md:flex space-x-6 absolute left-1/2 transform -translate-x-1/2">
+                <li><a href="#" className="hover:text-gray-400">Events</a></li>
+                <li><a href="#" className="hover:text-gray-400">Followed</a></li>
+                <li><a href="#" className="hover:text-gray-400">Discover</a></li>
             </ul>
-        </nav> 
+
+            {/* Right Side Icons */}
+            <div className="flex space-x-6">
+                <a href="#" className="hover:text-gray-400">
+                    <PlusIcon className="w-5 h-5 text-white" />
+                </a>
+                <a href="#" className="hover:text-gray-400">
+                    <SearchIcon className="w-5 h-5 text-white" />
+                </a>
+                <a href="#" className="hover:text-gray-400">
+                    <InboxIcon className="w-5 h-5 text-white" />
+                </a>
+                <a href="#" className="hover:text-gray-400">
+                    <UserIcon className="w-5 h-5 text-white" />
+                </a>
+            </div>
+        </nav>
     );
 };
+
 export default Navbar;

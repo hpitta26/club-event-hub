44c3f1ef473cc32dd6e36768dafec5c2c0ed060f
Alex Arguelles <alexarguelles87923@gmail.com>
2025-02-21 17:08:08 -0500

    fixing admin page

--- backend/restapi/admin.py
+++ backend/restapi/admin.py
@@ -4,7 +4,7 @@ from .models import Club, Student, Event, CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class ClubAdmin(admin.ModelAdmin):
-    list_display = ('get_email', 'description', 'spirit_rating')
+    list_display = ('get_email', 'club_name', 'description', 'spirit_rating')
     search_fields = ('user__email',)
 
     def get_email(self, obj):
@@ -12,7 +12,7 @@ class ClubAdmin(admin.ModelAdmin):
     get_email.short_description = 'Email'
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('get_email', 'major', 'graduation_year')
+    list_display = ('get_email', 'first_name', 'last_name', 'major', 'graduation_year')
     search_fields = ('user__email',)
 
     def get_email(self, obj):
@@ -27,8 +27,8 @@ class NewUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ("email", "is_staff", "is_active",)
-    list_filter = ("email", "is_staff", "is_active",)
+    list_display = ("email", "is_staff", "is_active", "role", "is_email_verified", "created_at", "updated_at")
+    list_filter = ("email", "is_staff", "is_active", "role", "is_email_verified", "created_at", "updated_at")
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),

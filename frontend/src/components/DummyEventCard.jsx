ac56447fafe40ad567ee92142f2e5b7a6cf030ab
hpitta <hpitta@students.cs.ubc.ca>
2025-03-04 01:51:32 -0500

    updated dummy logo, adjusted dummycard and clubprofile.
    created styles.css for removing scrollbar

--- frontend/src/components/DummyEventCard.jsx
+++ frontend/src/components/DummyEventCard.jsx
@@ -3,10 +3,10 @@ import dummyEventCardCover from "../assets/dummyEventCardCover.jpg";
 function DummyEventCard (){
     return (
         <div
-            className="container flex flex-col items-center p-28 bg-[#F5F5F5] w-72 h-95 px-4 py-5 rounded-3xl">
+            className="container flex flex-col items-center bg-[#D9D9D9] w-[190px] h-[215px] px-4 pt-5 rounded-[20px]">
             <img src={dummyEventCardCover} alt=""
-                 className="rounded-lg w-full h-48 object-cover object-bottom"/>
-            <p className="m-8">Event Card Placeholder</p>
+                 className="rounded-lg w-full h-38 object-cover object-bottom"/>
+            <p className="m-4">Some Event</p>
         </div>
 
     )

e8fd5fb6568be3d14d6a0a434903e3e873aac917
lucashurt <lucashurtado814@gmail.com>
2025-03-13 21:11:21 -0400

    Finalized styling, added error handling to the form and finalized patch request

--- frontend/src/pages/StudentSettings.jsx
+++ frontend/src/pages/StudentSettings.jsx
@@ -7,8 +7,12 @@ import {useNavigate} from "react-router-dom";
 
 function StudentSettings(){
 
-    const{userContext,Login} = useContext(UserContext);
-    const [globalEmail, setGlobalEmail] = useState(""); // Separate state for email at the top of the screen
+    const{userContext} = useContext(UserContext);
+    //globalEmail is used so that the eamil at the top of the screen doesnt change while the user types like the normal email formData will
+    const [globalEmail, setGlobalEmail] = useState("");
+    const[errors,setErrors] = useState({})
+    const [isLoading, setIsLoading] = useState(true);
+
     const [formData, setFormData] = useState({
         username: "",
         first_name: "",
@@ -41,83 +45,136 @@ function StudentSettings(){
                 console.error("Error fetching user data: ",error);
                 navigate("/*");
             }
+            finally {
+                setIsLoading(false)
+            }
         }
-        if (userContext){
+        if (userContext) {
             fetchUserDetails();
         }
-    },[])
+        else{
+            setIsLoading(false)
+        }
+
+    },[userContext])
 
     const handleChange = (e) =>{
         e.preventDefault();
         setFormData(prev=>({...prev,[e.target.name]:e.target.value}))
     }
 
+     const validateErrors = (data) =>{
+         const errors = {};
+         if(!data.first_name.trim()){
+             errors.first_name_blank = 'Cannot Leave First Name Blank';
+         }
+         if(!data.last_name.trim()){
+             errors.last_name_blank = 'Cannot Leave Last Name Blank';
+         }
+         if(!data.email.trim()){
+             errors.email_blank = 'Cannot Leave Email Blank';
+         }
+         if(!data.email.endsWith("edu")){
+             errors.invalid_email = 'Must be a .edu email';
+         }
+         if(!data.major.trim()){
+             errors.major_blank = 'Cannot Leave Major Blank';
+         }
+         if(data.graduation_year<2025){
+             errors.invalid_graduation_year = "Cannot Set Graduation Year before the current year";
+         }
+
+         return errors;
+     }
+
+    //used to see if the email was updated from its original state
+    const emailHasChanged = formData.email !== globalEmail;
+
     const handleSubmit = async(e) => {
         e.preventDefault();
+        const formErrors = validateErrors(formData);
+        setErrors(formErrors);
+
         const requestData = {
-                    user: {
-            email: formData.email,  // Ensure the email is inside the 'user' object
-        },
-        first_name: formData.first_name,
-        last_name: formData.last_name,
-        major: formData.major,
-        graduation_year: formData.graduation_year,
+            //since email is a field in the User object and not the Student object, we have to wrap it in a user object
+            //the patch request will NOT complete if the email in the User object has not changed due to our User serializers behavior
+            // due to this we conditionally pass the email through depending on if the email has changed or not
+            user: {
+                ...(emailHasChanged && {email: formData.email}) , // Ensure the email is inside the 'user' object
+            },
+            first_name: formData.first_name,
+            last_name: formData.last_name,
+            major: formData.major,
+            graduation_year: formData.graduation_year,
         };
 
-
-        try {
-            const response = await backend.patch(`/students/${userContext?.id}/`, requestData);
-            console.log("Settings updated " + response.data);
-            alert("Profile updated successfully!");
+        if(Object.keys(formErrors).length === 0) {
+            try {
+                const response = await backend.patch(`/students/${userContext?.id}/`, requestData);
+                console.log("Settings updated " + response.data);
+                alert("Profile updated successfully!");
+            } catch (error) {
+                console.error("Error updating user data: ", error);
+                alert("Failed to update profile. Please try again.");
+            }
         }
-        catch (error){
-            console.error("Error updating user data: ", error);
-            alert("Failed to update profile. Please try again.");
-        }
-
     }
 
+    if(isLoading){
+        return <div>Loading... </div>
+    }
     return(
-        <section className="min-h-screen bg-stone-900 flex flex-col items-center pb-20 pt-10">
-            <div className="w-full space-y-5 p-6 max-w-[860px] m-28 ">
-                <div className="flex items-end ml-24 mr-10">
-                    <img src={dummyInitLogo} alt="dummy picture" className="rounded-full h-32 mr-3"/>
+        <section className="min-h-screen bg-gray-200 flex flex-col justify-center items-center">
+            <div className="w-full space-y-5 p-6 max-w-[500px] ">
+                <div className="flex items-end">
                     <div>
-                        <p className="text-3xl text-white m-2"> {formData.username}</p>
-                        <p className="text-sm text-gray-400 m-2"> {globalEmail}</p>
-                        <div className="flex m-2">
+                        <div>
+                            <img src={dummyInitLogo} alt="dummy picture" className="rounded-full h-32"/>
                             <button onClick={() => console.log("X")}>
-                                <MdOutlineFileUpload className="text-gray-400 size-6 hover:text-gray-200 mr-2"/>
+                                <div className="flex m-2 text-black hover:text-gray-400">
+                                    <MdOutlineFileUpload className="size-6  mr-2"/>
+                                    <p> Upload Picture</p>
+                                </div>
                             </button>
-                            <p className="text-l text-gray-400"> Upload Picture</p>
                         </div>
                     </div>
+                    <div className="space-y-2">
+                        <p className="text-5xl text-black -mt-36"> {formData.username}</p>
+                        <p className="text-sm text-gray-500"> {globalEmail}</p>
+                    </div>
                 </div>
-                <div className="ml-24 mr-24 space-y-5">
+                <div className="space-y-5">
                     <div className="flex items-end justify-between ">
-                        <div className=" w-1/2 ">
-                            <p className="text-sm text-white"> First Name</p>
-                            <input name="first_name" onChange={handleChange} className="text-white w-11/12 bg-gray-500 h-8 p-2" placeholder={formData.first_name}/>
+                    <div className=" w-1/2 ">
+                            <p className="text-gray-500"> First Name</p>
+                            <input name="first_name" onChange={handleChange} className="text-black w-11/12 bg-gray-400 h-8 p-2 placeholder-gray-600" placeholder={formData.first_name}/>
+                            {errors.first_name_blank &&  <p className="text-red-500 text-xs italic"> {errors.first_name_blank} </p>}
                         </div>
                         <div className=" w-1/2 ">
-                            <p className="text-sm text-white"> Last Name</p>
-                            <input name="last_name" onChange={handleChange} className="text-white w-full bg-gray-500 h-8 p-2" placeholder={formData.last_name}/>
+                            <p className="text-gray-500"> Last Name</p>
+                            <input name="last_name" onChange={handleChange} className="text-black w-full bg-gray-400 h-8 p-2 placeholder-gray-600" placeholder={formData.last_name}/>
+                            {errors.last_name_blank &&  <p className="text-red-500 text-xs italic"> {errors.last_name_blank} </p>}
                         </div>
                     </div>
                     <div className=" w-full ">
-                        <p className="text-sm text-white"> Email</p>
-                        <input name="email" onChange={handleChange} className="text-white bg-gray-500 w-full h-8 p-2" placeholder={formData.email}/>
+                        <p className="text-gray-500"> Email</p>
+                        <input name="email" onChange={handleChange} className="text-black bg-gray-400 w-full h-8 p-2 placeholder-gray-600" placeholder={formData.email} />
+                        {errors.email_blank &&  <p className="text-red-500 text-xs italic"> {errors.email_blank} </p>}
+                        {errors.invalid_email &&  <p className="text-red-500 text-xs italic"> {errors.invalid_email} </p>}
+
                     </div>
                     <div className="w-full">
-                        <p className="text-sm text-white"> Major </p>
-                        <input name="major" onChange={handleChange} className="text-white bg-gray-500 w-full h-8 p-2"  placeholder={formData.major}/>
+                        <p className="text-gray-500"> Major </p>
+                        <input name="major" onChange={handleChange} className="text-black bg-gray-400 w-full h-8 p-2 placeholder-gray-600"  placeholder={formData.major}/>
+                        {errors.major_blank &&  <p className="text-red-500 text-xs italic"> {errors.major_blank} </p>}
                     </div>
                     <div className="w-full">
-                        <p className="text-sm text-white"> Grad Year</p>
-                        <input name="graduation_year" onChange={handleChange} type="number" className=" text-white bg-gray-500 w-full h-8 p-2" placeholder={formData.graduation_year}/>
+                        <p className="text-gray-500"> Graduation Year</p>
+                        <input name="graduation_year" onChange={handleChange} type="number" className=" text-black bg-gray-400 w-full h-8 p-2 placeholder-gray-600" placeholder={formData.graduation_year}/>
+                        {errors.invalid_graduation_year &&  <p className="text-red-500 text-xs italic"> {errors.invalid_graduation_year} </p>}
                     </div>
                     <div className="w-full">
-                        <button type= "submit" className="bg-white p-2" onClick={handleSubmit}>
+                        <button type="submit" className="bg-gray-300 p-2" onClick={handleSubmit}>
                             Save Changes
                         </button>
                     </div>

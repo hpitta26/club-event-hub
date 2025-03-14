e75a4415c8be396a2cdf2ecca285b405b3faaa00
hpitta <hpitta@students.cs.ubc.ca>
2025-03-13 22:51:01 -0400

    fixed indent

--- frontend/src/components/navbars/ClubNavbar.jsx
+++ frontend/src/components/navbars/ClubNavbar.jsx
@@ -24,7 +24,7 @@ const ClubNavbar = () => {
   const handleEventsClick = () => {
     setEventsClicked(true);
     setTimeout(() => setEventsClicked(false), 300);
-	navigate("/create-event");
+	  navigate("/create-event");
   };
 
   const handleLogout = () => {

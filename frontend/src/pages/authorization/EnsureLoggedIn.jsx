f7d7db9e4e25b56c9096200afe8573ef5f99cbf0
Alex Arguelles <alexarguelles87923@gmail.com>
2025-03-19 12:34:32 -0400

    fixing bug

--- frontend/src/pages/authorization/EnsureLoggedIn.jsx
+++ frontend/src/pages/authorization/EnsureLoggedIn.jsx
@@ -5,16 +5,23 @@ import PropTypes from 'prop-types';
 
 export const EnsureLoggedIn = ({ role = "CheckLoggedIn" }) => {
     const { userContext } = useContext(UserContext);
-
-    if (role === "CheckLoggedIn") {
-        return <Navigate to="/login" />;
+    if (userContext) {
+        if (role === "NotLoggedIn") {
+            return <Navigate to="/" />;
+        };
+    
+        const roles = userContext['role'];
+        
+        if (Array.isArray(roles) && !roles.includes(role)) return <Navigate to="/" />;
+    } else {
+        if (role === "CheckLoggedIn") {
+            return <Navigate to="/login" />;
+        };
     };
-
-    if (!userContext.role.contains(role)) return <Navigate to="/" />;
 
     return <Outlet />;
 };
 
 EnsureLoggedIn.propTypes = {
-    role: PropTypes.oneOf(["CLUB", "STUDENT", "CheckLoggedIn"]),
+    role: PropTypes.oneOf(["CLUB", "STUDENT", "CheckLoggedIn", "NotLoggedIn"]),
 };

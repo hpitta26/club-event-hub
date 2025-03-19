8bb41de9cf058f6d50c0fec866e9aa2146e30bfa
hpitta <hpitta@students.cs.ubc.ca>
2025-03-19 16:58:25 -0400

    adjusted protected route logic

--- frontend/src/pages/authentication/Login.jsx
+++ frontend/src/pages/authentication/Login.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useContext, useState } from "react";
 import backend from "../../components/backend.jsx";
 import { UserContext } from "../../context/UserContext.jsx";
@@ -66,3 +67,73 @@ function Login() {
 }
 
 export default Login;
+=======
+import { useContext, useState } from "react";
+import backend from "../../components/backend.jsx";
+import { UserContext } from "../../context/UserContext.jsx";
+import { useNavigate } from "react-router-dom";
+import FormContainer from "../../components/FormContainer.jsx";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+  const fields = [
+    { name: "email", type: "email", label: "Email" },
+    { name: "password", type: "password", label: "Password" },
+  ];
+  const { Login } = useContext(UserContext);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await backend.post("/login/", formData);
+      // console.log(response);
+      // console.log(response.data.user);
+      if (response.status === 200) {
+        console.log("logging user in...");
+        await new Promise((resolve) => {
+          Login(response.data.user);
+          resolve(); // Ensure the context is updated before proceeding
+        });
+        if (response.data.user['role'].includes("STUDENT")) {
+          navigate('/discover');
+        } else {
+          navigate("/analytics");
+        }
+	    } else {
+        setError(response.message);
+      }
+    } catch (err) {
+      console.error("Login error:", err.response ? err.response.data.error : err.message);
+      setError("Login failed. Please try again.");
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  return (
+    <div className="min-h-screen bg-stone-900 flex justify-center items-center pt-10">
+      <FormContainer
+        title={"Welcome to GatherU"}
+        subtitle="University Events at a Glance"
+        fields={fields}
+        formData={formData}
+        handleChange={handleChange}
+        onSubmit={handleSubmit}
+      >
+        Submit
+      </FormContainer>
+    </div>
+  );
+}
+
+export default Login;
+>>>>>>> 5a06f19 (adjusted protected route logic)
--- frontend/src/pages/authentication/Logout.jsx
+++ frontend/src/pages/authentication/Logout.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useContext, useEffect } from 'react';
 import backend from '../../components/backend.jsx';
 import { UserContext } from '../../context/UserContext.jsx';
@@ -26,3 +27,33 @@ function Login() {
 };
 
 export default Login;
+=======
+import { useContext, useEffect } from 'react';
+import backend from '../../components/backend.jsx';
+import { UserContext } from '../../context/UserContext.jsx';
+import { useNavigate } from 'react-router-dom';
+
+
+function Login() {
+    const { Logout } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        backend.get('/logout/');
+        Logout();
+        setTimeout(() => {
+            navigate('/');
+        }, 1000);
+    }, []);
+    
+    return (
+        <section className="min-h-screen bg-stone-900 flex justify-center items-center pt-10">
+            <div>
+                <h1 className='text-4xl text-pink-100 hover:text-pink-200'>Successfully logged out!</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
+>>>>>>> 5a06f19 (adjusted protected route logic)

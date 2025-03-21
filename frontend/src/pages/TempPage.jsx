933e2ede2877364747df7db791858866025e7867
Bryan A Fernandez <b305ryan123@gmail.com>
2025-03-21 10:36:54 -0400

    Created Event Card Details Component

--- frontend/src/pages/TempPage.jsx
+++ frontend/src/pages/TempPage.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import EventCard from '../components/EventCard'
 import EventModal from '../components/EventModal'
+import EventDetailsCard from '../components/EventDetailsCard'
 
 
 // Temporary Page to visualize the EventCard component.
-
+// http://localhost:5173/temp
 function TempPage() {
   return (
     <div className='flex flex-col items-center justify-center bg-slate-950 p-10 min-h-screen pt-10'>
-      <EventCard />
-      <EventModal />
+<EventDetailsCard
+        title="Intro To LLMs"
+        club="INIT FIU"
+        day="Tuesday, March 19th"
+        time="6:00 PM - 8:00 PM EST"
+        description="Join us for an introductory workshop on Large Language Models (LLMs). Learn about the fundamentals of LLMs, their applications, and get hands-on experience with popular models. Perfect for beginners and intermediate developers interested in AI."
+        universityName="Florida International University"
+        roomLocation="PG 6 - 106"
+        attendees={191}
+
+/>
     </div>
   )
 }

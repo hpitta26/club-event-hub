5a870e62e05ee9afa9087107c509fc9af91bbdf2
lucashurt <lucashurtado814@gmail.com>
2025-04-10 10:58:43 -0400

    removed redundant logic in recommended events

--- frontend/src/components/discover/RecommendedEvents.jsx
+++ frontend/src/components/discover/RecommendedEvents.jsx
@@ -6,25 +6,24 @@ const RecommendedEvents = ({ events }) => {
       <div>
           <h1 className="text-lg font-bold mb-4">Recommended Events</h1>
           <div className="flex overflow-x-auto gap-x-4 h-72">
-            {events.length > 0 &&
-                      events.map((event) => (
-                              <div key={event.id}>
-                                  <NewEventCard
-                                      id={event.id}
-                                      title={event.title}
-                                      description={event.description}
-                                      date={event.start_time}
-                                      host={event.host}
-                                      location={event.location}
-                                      attendees={event.attending}
-                                      capacity={event.capacity - event.attending}
-                                      // coverImage={event.coverImage}
-                                      // hostLogo={event.hostLogo}
-                                      is_rsvped={event.is_rsvped}
-                                  />
-                              </div>
-                          )
-                      )}
+              {events.map((event) => (
+              <div key={event.id}>
+                  <NewEventCard
+                      id={event.id}
+                      title={event.title}
+                      description={event.description}
+                      date={event.start_time}
+                      host={event.host}
+                      location={event.location}
+                      attendees={event.attending}
+                      capacity={event.capacity - event.attending}
+                      // coverImage={event.coverImage}
+                      // hostLogo={event.hostLogo}
+                      is_rsvped={event.is_rsvped}
+                  />
+              </div>
+              )
+              )}
           </div>
       </div>
   );

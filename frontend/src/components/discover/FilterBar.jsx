79516beb243f0e82543afe4a8b5671e671495c15
Lorenzo Montero <lorenzomontero@MacBookAir.attlocal.net>
2025-04-18 01:11:27 -0400

    added hover effect and changed the color on the filter buttons

--- frontend/src/components/discover/FilterBar.jsx
+++ frontend/src/components/discover/FilterBar.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
 
 const Filterbar = ({ categories, onFilterSelect }) => {
-    const [activeFilter, setActiveFilter] = useState("All");
+  const [activeFilter, setActiveFilter] = useState("All");
 
-    const handleFilterClick = (category) => {
-        setActiveFilter(category);
-        onFilterSelect(category);
-    };
+  const handleFilterClick = (category) => {
+    setActiveFilter(category);
+    onFilterSelect(category);
+  };
 
-    return (
-        <div className="mb-1 flex flex-wrap gap-[10px]">
-            {categories.map((category) => (
-                <button
-                    key={category}
-                    className={`px-3 text-[16px] rounded-md border-[1.5px] border-black transition text-sm md:text-base ${
-                        activeFilter === category
-                            ? "bg-[#CFCCCC] shadow-[2px_2px_0px_#000000]"
-                            : "bg-white"
-                    }`}
-                    onClick={() => handleFilterClick(category)}
-                >
-                    {category}
-                </button>
-            ))}
-        </div>
-    );
+  return (
+    <div className="mb-1 flex flex-wrap gap-[10px]">
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={`px-3 text-[16px] rounded-md border-[1.5px] border-black transition text-sm md:text-base hover:bg-[#ffe4f4] hover:shadow-[2px_2px_0px_#000000] ${
+            activeFilter === category
+              ? "bg-[#fec8e9] shadow-[2px_2px_0px_#000000]"
+              : "bg-white"
+          }`}
+          onClick={() => handleFilterClick(category)}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Filterbar;

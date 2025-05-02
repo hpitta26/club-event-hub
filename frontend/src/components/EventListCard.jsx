88793ce3b35017806d5e3e285816703f17c41cd2
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air-3.local>
2025-05-02 15:19:45 -0400

    Added the shadow animation the EventListCard and the EventModalCard

--- frontend/src/components/EventListCard.jsx
+++ frontend/src/components/EventListCard.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import dummyEventCardCover from "../assets/dummyEventCardCover.jpg";
-import { truncate } from '../utils/truncate';
-import { dateFormat } from '../utils/dates';
+import { truncate } from "../utils/truncate";
+import { dateFormat } from "../utils/dates";
 
-function EventListCard({ 
-  title = "Untitled Event", 
-  date = "TBD", 
+function EventListCard({
+  title = "Untitled Event",
+  date = "TBD",
   capacity = "N/A",
-  host = "Unknown Host", 
-  image = dummyEventCardCover 
+  host = "Unknown Host",
+  image = dummyEventCardCover,
 }) {
-
   const spotsLeft = capacity === "N/A" ? "N/A" : capacity;
   let spotsLeftColor = "#35A25D"; // Default color for 30+ spots
 
@@ -21,21 +20,21 @@ function EventListCard({
   }
 
   return (
-    <div className="container flex gap-3 w-full px-4 py-4 bg-[#F0EFEB] rounded-lg hover:bg-[#E0DFDB] transition border-2 border-black">
-      <img src={image} alt="Event" className="w-[104px] h-[104px] rounded-lg object-cover border-2 border-black" />
+    <div className="container flex gap-3 w-full px-4 py-4 bg-[#F0EFEB] rounded-lg hover:bg-[#E0DFDB] hover:shadow-[3px_3px_0px_#000000] transition border-2 border-black">
+      <img
+        src={image}
+        alt="Event"
+        className="w-[104px] h-[104px] rounded-lg object-cover border-2 border-black"
+      />
       <div className="ml-1 flex flex-col gap-3 justify-center">
         {/* Event Date */}
-        <p className="text-black font-semibold text-xs">
-          {dateFormat(date)}
-        </p>
+        <p className="text-black font-semibold text-xs">{dateFormat(date)}</p>
         {/* Event Title */}
         <p className="text-black text-sm font-bold leading-none">
-          {truncate(title, 50)} 
+          {truncate(title, 50)}
         </p>
-        {/* Event Host */} 
-        <p className="text-black font-semibold text-xs">
-          Hosted by: {host}
-        </p>
+        {/* Event Host */}
+        <p className="text-black font-semibold text-xs">Hosted by: {host}</p>
         {/* Spots Left */}
         <p className="text-[12.5px]" style={{ color: spotsLeftColor }}>
           {spotsLeft} spots left
